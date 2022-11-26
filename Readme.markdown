705b42d6cc3c14d14e844254adcb0439b8ac0393
austinbasu <72290934+austinbasu@users.noreply.github.com>
2022-11-25 21:38:32 -0700

    Project upload

---
+++ Readme.markdown
@@ -0,0 +1,9 @@
+# Election Vote Visualizer Full Stack App #
+
+
+This is a quick app that uses PHP as the backend to interact with a PostgresSQL database and jquery javascript as the frontend. 
+
+![](images/pic.png?raw=true)
+
+
+Basic elements for the US Map were used from [here](https://github.com/NewSignature/us-map)
---
+++ backend/get_electorial.php
@@ -0,0 +1,20 @@
+<?php
+	$cn = pg_connect("host=localhost port=5432 dbname=united_states
+	user=postgres password=1234");
+
+	## Running an SQL injection escape string
+	$year = pg_escape_string($_GET['year']);
+
+	$query = "SELECT * FROM public.electorial WHERE year=".$year.";";
+
+	$result = pg_query($cn, $query);
+
+	$data = array();
+	while($row = pg_fetch_object($result)){
+		$data[] = $row;
+	}
+
+	//now print the data
+	print json_encode($data);
+
+?>
